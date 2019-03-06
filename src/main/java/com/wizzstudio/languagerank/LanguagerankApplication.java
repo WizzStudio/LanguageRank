7b5d6342708cd9fcb956e323a8888bea615dacac
ben286 <benwen931@gmail.com>
2019-03-06 18:30:03 +0800

    first commit

---
+++ src/main/java/com/wizzstudio/languagerank/LanguagerankApplication.java
@@ -0,0 +1,13 @@
+package com.wizzstudio.languagerank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguagerankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanguagerankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wizzstudio/languagerank/LanguagerankApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wizzstudio.languagerank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LanguagerankApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

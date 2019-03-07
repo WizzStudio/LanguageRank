46f9e6489bbb48f1dc2694a2428f3ce6fd78c4f9
ben286 <benwen931@gmail.com>
2019-03-07 20:45:02 +0800

    second commit

---
+++ src/main/java/com/wizzstudio/languagerank/domin/Company.java
@@ -0,0 +1,38 @@
+package com.wizzstudio.languagerank.domin;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+public class Company {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @NotNull
+    private String languageName;
+
+    @NotNull
+    private String companyName;
+
+//      平均薪资
+    private Integer companyOrdSalary;
+//      最高薪资
+    private Integer companyMaxSalary;
+//      最低薪资
+    private Integer companyMinSalary;
+//      岗位人数
+    private Integer companyPostNumber;
+//      薪资最高的岗位
+    private String companyMaxSalaryPost;
+//      薪资最低的岗位
+    private String companyMinSalaryPost;
+
+}

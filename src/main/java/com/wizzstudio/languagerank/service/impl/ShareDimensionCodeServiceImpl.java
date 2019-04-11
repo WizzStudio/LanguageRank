cd2e57d840fc274d39018cdc04df3d54985418a9
Uoylive <1647565256@qq.com>
2019-04-11 12:04:47 +0800

    改分享二维码

--- src/main/java/com/wizzstudio/languagerank/service/impl/ShareDimensionCodeServiceImpl.java
+++ src/main/java/com/wizzstudio/languagerank/service/impl/ShareDimensionCodeServiceImpl.java
@@ -9,17 +9,24 @@ import java.util.List;
 @Service
 public class ShareDimensionCodeServiceImpl implements ShareDimensionCodeService {
 
-    private static final String page1 = "Http://pgrk.wizzstudio.com/";
-    private static final String page2 = "Http://pgrk.wizzstudio.com/";
-    private static final String page3 = "Http://pgrk.wizzstudio.com/";
+//    语言热度帮
+    private static final String pageIndex = "Http://pgrk.wizzstudio.com/";
+//    公司需求页
+    private static final String detailDemandHome = "Http://pgrk.wizzstudio.com/";
+//    语言主页
+    private static final String detailLangHome = "Http://pgrk.wizzstudio.com/";
+//    语言详情
+    private static final String detailLangDetail = "Http://pgrk.wizzstudio.com/";
+
 
     @Override
     public List<String> getDimensionCode() {
 
         List<String> list = new ArrayList<>();
-        list.add(page1);
-        list.add(page2);
-        list.add(page3);
+        list.add(pageIndex);
+        list.add(detailDemandHome);
+        list.add(detailLangHome);
+        list.add(detailLangDetail);
 
         return list;
     }
